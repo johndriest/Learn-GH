73064c2cc8dd1708829718018fce7d8c63cc7b4f
John Driest <john.driest@mac.com>
2021-12-10 15:19:07 +0700

    Initial Commit

---
+++ Learning GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning GH
+//
+//  Created by John Driest on 10/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
